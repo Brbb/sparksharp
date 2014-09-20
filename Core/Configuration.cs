dac7179e0c4c9d6e1229e718ad3f444977e556cf
Brbb <franxster@gmail.com>
2014-09-20 20:15:55 +0200

    Update Configuration.cs

--- Core/Configuration.cs
+++ Core/Configuration.cs
@@ -4,17 +4,20 @@ namespace Spark
 {
 	public class Configuration
 	{
-		private static String deviceId = "<YOUR_DEVICE_ID>";
-		private static String accessToken = "<YOUR_ACCESS_TOKEN>";
-
 		// TODO: Read deviceId and accessToken from local storage file or login result.
 		public Configuration()
 		{
 
 		}
 
-		public static String DeviceID { get {return deviceId;}}
-		public static String AccessToken { get {return accessToken;}}
+		public static String DeviceID {
+			get;
+			set;
+		}
+		public static String AccessToken { 
+			get;
+			set;
+		}
 	}
 }
 
