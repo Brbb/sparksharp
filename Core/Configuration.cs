7d3effda6741ae15fac0b30951e9ae1864e143a5
Brbb <francesco@kiwi.local>
2014-09-20 19:52:09 +0200

    Added Configuration

---
+++ Core/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Spark
+{
+	public class Configuration
+	{
+		private static String deviceId = "<YOUR_DEVICE_ID>";
+		private static String accessToken = "<YOUR_ACCESS_TOKEN>";
+
+		// TODO: Read deviceId and accessToken from local storage file or login result.
+		public Configuration()
+		{
+
+		}
+
+		public static String DeviceID { get {return deviceId;}}
+		public static String AccessToken { get {return accessToken;}}
+	}
+}
+
